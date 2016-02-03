b00fd963f9b5f102ed56e1c7e403ff4300481be1
RaphaelGyory <xellessprl@gmail.com>
2016-02-03 20:24:38 +0100

    Beta release

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ setup(
          'six',
     ],
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
